bb7daa309c2910ebf6450ba0a58ac7d1df01d819
nchandika1 <nchandika@gmail.com>
2018-01-28 13:03:05 -0800

    Small restructuring of directories

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import hello from '../../../assets/images/hello.png';
+import '../../Portfolio.css';
+
+const Home = () => {
+	return (<div className="Content-style"><img className="Home-content" src={hello} /></div>);
+};
+
+export default Home;
