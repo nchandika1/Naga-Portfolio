45dde1b7d60a075edbf3f0322fc0b3c908e57ba3
nchandika1 <nchandika@gmail.com>
2018-01-28 15:45:33 -0800

    Minor changes in About page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -11,17 +11,26 @@ const Home = () => {
 				<p className="About-title">Full stack developer</p>
 				
 			</div>
-			<p className="About-content"><p>Having just graduated from UC Berkeley Extension program on Full Stack Web Development,
+			<div className="About-content"><p>Having just graduated from UC Berkeley Extension program on Full Stack Web Development,
 					I am looking for an opportunity to design, develop and test web applications.</p>
 					<p>I am well versed in <i>Javascript, jQuery, React, Node.js.</i>   I built many web apps using these technologies and various DBs such as MySQL, Sequelize, MongoDB.
 					</p>
 					<p>I am especially proud of my final project that showcases <i>React, Node.js, Chart, Axios, JSX, Sequelize</i> etc.
-					Please do checkout my Portfolio under Blog.</p>
+					Please do checkout my Portfolio under <span style={{color: '#66dbf9'}}>Blog.</span></p>
 					<p>Prior to this, I have done years of embedded software development in the area of Networking at <em>Cisco</em> and <em>Apple</em>.
 					I hold multiple patents in the area of Layer 2 Switching Software for Cisco Catalyst switches.
-					Be sure to checkout the details on the Contact page.
+					Details of my work experience are in my <em>Resume</em> on the <span style={{color: '#66dbf9'}}>Contact</span> page.
 					</p>
-			</p>
+			</div>
+			<div className="About-content">
+				<p className="Edu-title">Educational Background:</p>
+				<div>
+				• UC Berkeley Extension:  Full Stack Web Development program (Oct 2017 - Jan 2018) <br />
+				• Hackbright Academy for Women Coders: Intro to Python (Sep 2017 - Oct 2017)<br />
+				• Arizona State University: Masters in Computer Science (Aug 1993 - May 1995)<br />
+				• Osmania University, College of Engg: Bachelors in Computer Science and Engg (Aug 1989 - June 1993)<br />	
+				</div>			
+			</div>
 		</div>
 	);
 }
