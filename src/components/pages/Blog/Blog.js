18858b476e849d47e1883b2b56300314ec6853e1
nchandika1 <nchandika@gmail.com>
2018-01-29 19:45:25 -0800

    ready for deploy

--- src/components/pages/Blog/Blog.js
+++ src/components/pages/Blog/Blog.js
@@ -2,8 +2,26 @@ import React from 'react';
 import './Blog.css';
 
 const Blog = () => {
-	console.log("Inside Blog");
-	return <div className="Blog-content">Blog: Under Construction</div>
+	return (
+		<div className="Blog-content">
+			<div className="Project">
+				<table className="Table-Style">
+					<tr>
+						<th>Project Name</th>
+						<th>Description</th>
+						<th>Technologies Used</th>
+						<th>Deployment</th>
+					</tr>
+						<td>HOPE APP</td>
+						<td>App to keep track of individual donations in $$ and Hours</td>
+						<td>React, Node, JSX, Chart, Axiios, Sequelize</td>
+						<td><a href="https://shielded-retreat-54823.herokuapp.com/" target="_blank">https://shielded-retreat-54823.herokuapp.com/</a></td>
+					<tr>
+					</tr>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default Blog;
